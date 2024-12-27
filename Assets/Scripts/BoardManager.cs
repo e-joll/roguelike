bc097adf4bf89359b0b3008be212e519c59b9a4b
e-joll <eleonore.jollivet@gmail.com>
2024-12-27 21:32:08 +0100

    ✨ add random cell display with food prefabs
    - Implemented random placement of cells containing food prefabs.
    - Food prefabs are displayed in random cells on the board.

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -6,23 +7,29 @@ public class BoardManager : MonoBehaviour
     public class CellData
     {
         public bool Passable;
+        public GameObject ContainedObject;
     }
     
 
     private CellData[,] m_BoardData;
     private Tilemap m_Tilemap;
     private Grid m_Grid;
+    private List<Vector2Int> m_EmptyCellsList;
 
     public int Width;
     public int Height;
     public Tile[] GroundTiles;
     public Tile[] BlockingTiles;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject[] FoodPrefab;
+    public int minFoodCount;
+    public int maxFoodCount;
+    
     public void Init()
     {
         m_Tilemap = GetComponentInChildren<Tilemap>();
         m_Grid = GetComponentInChildren<Grid>();
+        m_EmptyCellsList = new List<Vector2Int>();
   
         m_BoardData = new CellData[Width, Height];
 
@@ -42,11 +49,19 @@ public class BoardManager : MonoBehaviour
                 {
                     tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
                     m_BoardData[x, y].Passable = true;
+                    
+                    //this is a passable empty cell, add it to the list!
+                    m_EmptyCellsList.Add(new Vector2Int(x, y));
                 }
 
                 m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
         }
+        
+        //remove the starting point of the player! It's not empty, the player is there
+        m_EmptyCellsList.Remove(new Vector2Int(1, 1));
+        
+        GenerateFood();
     }
     
     public Vector3 CellToWorld(Vector2Int cellIndex)
@@ -65,9 +80,20 @@ public class BoardManager : MonoBehaviour
         return m_BoardData[cellIndex.x, cellIndex.y];
     }
 
-    // Update is called once per frame
-    void Update()
+    void GenerateFood()
     {
-        
+        int foodCount = Random.Range(minFoodCount, maxFoodCount+1);
+        for (int i = 0; i < foodCount; ++i)
+        {
+            int randomIndex = Random.Range(0, m_EmptyCellsList.Count);
+            Vector2Int coord = m_EmptyCellsList[randomIndex];
+      
+            m_EmptyCellsList.RemoveAt(randomIndex);
+            CellData data = m_BoardData[coord.x, coord.y];
+            int randomFoodPrefabIndex = Random.Range(0, FoodPrefab.Length);
+            GameObject newFood = Instantiate(FoodPrefab[randomFoodPrefabIndex]);
+            newFood.transform.position = CellToWorld(coord);
+            data.ContainedObject = newFood;
+        }
     }
 }
