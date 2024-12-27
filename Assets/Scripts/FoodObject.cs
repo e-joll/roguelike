60f49390a22583437dd4cc8507b22e9aa1716782
e-joll <eleonore.jollivet@gmail.com>
2024-12-27 22:51:31 +0100

    ✨ add scoring property to FoodObject and rename smallfood2 to mediumfood
    - Added a scoring property to FoodObject to control the points given.
    - Renamed smallfood2 prefab to mediumfood to indicate it gives more points than smallfood.

--- Assets/Scripts/FoodObject.cs
+++ Assets/Scripts/FoodObject.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class FoodObject : CellObject
 {
+    public int AmountGranted = 10;
     public override void PlayerEntered()
     {
         Destroy(gameObject);
       
         //increase food
-        Debug.Log("Food increased");
+        GameManager.Instance.ChangeFood(AmountGranted);
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -39,8 +39,12 @@ public class GameManager : MonoBehaviour
     
     void OnTurnHappen()
     {
-        m_FoodAmount -= 1;
+        ChangeFood(-1);
+    }
+    
+    public void ChangeFood(int amount)
+    {
+        m_FoodAmount += amount;
         m_FoodLabel.text = "Food : " + m_FoodAmount;
-        Debug.Log("Current amount of food : " + m_FoodAmount);
     }
 }
